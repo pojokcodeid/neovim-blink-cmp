aac60a16c51a26b9ab8b76b5ab251becb44c63e8
Pojok Code <pojokcodeid@gmail.com>
2025-08-03 10:58:47 +0700

    enc: change bufferline to cookie line

--- lua/pcode/plugins/bufferline.lua
+++ lua/pcode/plugins/bufferline.lua
@@ -1,11 +1,66 @@
 return {
-	"akinsho/bufferline.nvim",
-	branch = "main",
-	event = { "BufRead", "InsertEnter", "BufNewFile" },
-	dependencies = "pojokcodeid/auto-bufferline.nvim",
-	config = function()
-		vim.opt.termguicolors = true
-		local config = require("auto-bufferline").config()
-		require("bufferline").setup(config)
+	"willothy/nvim-cokeline",
+	event = { "BufRead", "BufNewFile" },
+	opts = function()
+		local yellow = vim.g.terminal_color_3
+		local hlgroups = require("cokeline.hlgroups")
+		local hl_attr = hlgroups.get_hl_attr
+		return {
+			sidebar = {
+				filetype = { "NvimTree", "neo-tree" },
+				components = {
+					{
+						text = function(buf)
+							return buf.filetype
+						end,
+						fg = yellow,
+						bg = function()
+							return hl_attr("NvimTreeNormal", "bg")
+						end,
+						bold = true,
+					},
+				},
+			},
+			default_hl = {
+				fg = function(buffer)
+					return buffer.is_focused and hl_attr("Normal", "fg") or hl_attr("Comment", "fg")
+				end,
+				bg = function(buffer)
+					return buffer.is_focused and hl_attr("ColorColumn", "bg") or hl_attr("Normal", "bg")
+				end,
+			},
+			components = {
+				{
+					text = function(buffer)
+						if buffer.is_focused then
+							return "▎" .. buffer.devicon.icon
+						else
+							return "｜" .. buffer.devicon.icon
+						end
+					end,
+					fg = function(buffer)
+						return buffer.devicon.color
+					end,
+				},
+				{
+					text = " ",
+				},
+				{
+					text = function(buffer)
+						return buffer.filename .. " "
+					end,
+					style = function(buffer)
+						return buffer.is_focused and "bold" or nil
+					end,
+					italic = function(buffer)
+						return buffer.is_focused and true or nil
+					end,
+				},
+				{
+					text = "󰅖 ",
+					delete_buffer_on_left_click = true,
+				},
+			},
+		}
 	end,
 }
