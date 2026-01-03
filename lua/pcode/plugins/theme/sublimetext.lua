10404bd0fd4246bfdb0b633b0a0c1c19b60f833b
Pojok Code <pojokcodeid@gmail.com>
2026-01-03 09:32:20 +0700

    enc: update config nvim-juliana

--- lua/pcode/plugins/theme/sublimetext.lua
+++ lua/pcode/plugins/theme/sublimetext.lua
@@ -1,19 +1,21 @@
 return {
-  "pojokcodeid/nvim-juliana",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-    vim.api.nvim_create_autocmd("ColorScheme", {
-      pattern = "*",
-      callback = function()
-        -- get colors
-        local colors = require("nvim-juliana").colors()
-        -- custom hilights
-        local hi = vim.api.nvim_set_hl
-        hi(0, "FoldColumn", { bg = colors.bg2 })
-        hi(0, "MasonBackdrop", { link = "NormalFloat" })
-      end,
-    })
-  end,
+	"pojokcodeid/nvim-juliana",
+	lazy = false,
+	priority = 1000,
+	opts = {},
+	config = function()
+		vim.api.nvim_create_autocmd("ColorScheme", {
+			pattern = "*",
+			callback = function()
+				-- get colors
+				local colors = require("nvim-juliana").colors()
+				-- custom hilights
+				local hi = vim.api.nvim_set_hl
+				hi(0, "FoldColumn", { bg = colors.bg2 })
+				hi(0, "MasonBackdrop", { link = "NormalFloat" })
+				hi(0, "TabLineFill", { link = "NormalFloat" })
+				hi(0, "ColorColumn", { bg = "#3f4750" })
+			end,
+		})
+	end,
 }
--- lua/pcode/user/default.lua
+++ lua/pcode/user/default.lua
@@ -80,7 +80,7 @@ pcode.extras = {
 -- github = "github_dark_dimmed",
 
 pcode.themes = {
-  dracula = "dracula"
+  sublimetext = "juliana"
 }
 -- activate config transparent_bg
 pcode.transparent = false
