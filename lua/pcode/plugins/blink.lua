bc6a065950bf056e64b78dba4c227edb2fa75418
SF7 Repository <pojokcodeid@gmail.com>
2026-09-19 19:33:50 +0700

    update config luasnip build cmake

--- lua/pcode/plugins/blink.lua
+++ lua/pcode/plugins/blink.lua
@@ -3,7 +3,7 @@ return {
 	event = { "InsertEnter", "CmdlineEnter" },
 	dependencies = {
 		"rafamadriz/friendly-snippets",
-		{ "L3MON4D3/LuaSnip", version = "v2.*" },
+		{ "L3MON4D3/LuaSnip", version = "v2.*", build = "make install_jsregexp" },
 	},
 	version = "1.*",
 	-- build = "cargo build --release",
