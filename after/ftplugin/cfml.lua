f70a88d8220379a74e1ad7b21076aacc5d2856d7
SF7 Repository <pojokcodeid@gmail.com>
2026-09-20 21:54:49 +0700

    enc: add auto close tag cor cftag

--- after/ftplugin/cfml.lua
+++ after/ftplugin/cfml.lua
@@ -48,6 +48,113 @@ require("Comment").setup({
 	end,
 })
 
+-- config for auto close tag
+------------------------------------------------------------
+-- Daftar tag yang butuh pasangan penutup (whitelist)
+-- Tag self-closing (cfset, cfparam, cfinclude, dll) SENGAJA tidak
+-- dimasukkan supaya tidak ikut auto-close.
+------------------------------------------------------------
+local closing_tags = {
+	cfif = true,
+	cfelseif = true,
+	cfelse = true,
+	cfloop = true,
+	cfoutput = true,
+	cftry = true,
+	cfcatch = true,
+	cffinally = true,
+	cfswitch = true,
+	cfcase = true,
+	cfdefaultcase = true,
+	cffunction = true,
+	cfcomponent = true,
+	cflock = true,
+	cftransaction = true,
+	cfsavecontent = true,
+	cfquery = true,
+	cfstoredproc = true,
+	cfscript = true,
+	cfmail = true,
+	cfhttp = true,
+	cfthread = true,
+	cfzip = true,
+	cfpdf = true,
+	cfdocument = true,
+}
+
+------------------------------------------------------------
+-- Auto-close: ketik ">" setelah "<cfif ..." -> jadi "<cfif ...></cfif>"
+------------------------------------------------------------
+local function cfml_auto_close_on_gt()
+	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+	local line = vim.api.nvim_get_current_line()
+	local before = line:sub(1, col)
+
+	-- skip kalau tag ditulis self-closing, misal: <cfif ... />
+	if before:match("/%s*$") then
+		vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { ">" })
+		vim.api.nvim_win_set_cursor(0, { row, col + 1 })
+		return
+	end
+
+	local tagname = before:match("<(%a[%w_]*)[^<>]*$")
+
+	vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { ">" })
+	vim.api.nvim_win_set_cursor(0, { row, col + 1 })
+
+	if tagname and closing_tags[tagname:lower()] then
+		local closing = "</" .. tagname .. ">"
+		vim.api.nvim_buf_set_text(0, row - 1, col + 1, row - 1, col + 1, { closing })
+		vim.api.nvim_win_set_cursor(0, { row, col + 1 })
+	end
+end
+
+vim.keymap.set("i", ">", cfml_auto_close_on_gt, {
+	buffer = true,
+	desc = "Auto close CFML tag",
+})
+
+------------------------------------------------------------
+-- Expand: Enter di antara tag pembuka & penutup -> jadi 3 baris
+------------------------------------------------------------
+local function cfml_expand_on_cr()
+	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+	local line = vim.api.nvim_get_current_line()
+	local before = line:sub(1, col)
+	local after = line:sub(col + 1)
+
+	local is_after_open_tag = before:match(">%s*$") ~= nil
+	local closing_tagname = after:match("^</(%a[%w_]*)>")
+
+	if is_after_open_tag and closing_tagname then
+		local current_indent = line:match("^%s*") or ""
+		local shiftwidth = vim.bo.shiftwidth > 0 and vim.bo.shiftwidth or vim.bo.tabstop
+		local extra_indent
+		if vim.bo.expandtab then
+			extra_indent = string.rep(" ", shiftwidth)
+		else
+			extra_indent = "\t"
+		end
+		local middle_indent = current_indent .. extra_indent
+
+		vim.api.nvim_buf_set_lines(0, row - 1, row, false, {
+			before,
+			middle_indent,
+			current_indent .. after,
+		})
+
+		vim.api.nvim_win_set_cursor(0, { row + 1, #middle_indent })
+	else
+		local keys = vim.api.nvim_replace_termcodes("<CR>", true, false, true)
+		vim.api.nvim_feedkeys(keys, "n", false)
+	end
+end
+
+vim.keymap.set("i", "<CR>", cfml_expand_on_cr, {
+	buffer = true,
+	desc = "Expand CFML tag block on Enter",
+})
+
 -- config for snippets
 local ls = require("luasnip")
 
