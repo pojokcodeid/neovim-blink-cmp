64e6cf39d53bdab84757c5d9565d3d0e1b763ea3
SF7 Repository <pojokcodeid@gmail.com>
2026-09-20 11:05:08 +0700

    change lucee to cfml

--- lua/pcode/plugins/lsp/cfml.lua
+++ lua/pcode/plugins/lsp/cfml.lua
@@ -1,10 +1,10 @@
 -- lua/pcode/plugins/lsp/lucee.lua
 -- download dari https://github.com/cfmleditor/cfmleditor-lsp
 -- pastikan file sudah di allow
-local binary_path = vim.fn.expand("~/.config/nvim/lua/pcode/plugins/lsp/lib/cfmleditor-lsp")
+local binary_path = vim.fn.expand(vim.fn.stdpath("config") .. "/lua/pcode/plugins/lsp/lib/cfmleditor-lsp")
 return {
 	cmd = { binary_path },
-	filetypes = { "cfml", "cfscript" },
+	filetypes = { "cfml", "cfscript", "cfm", "cfc" },
 	single_file_support = true,
 	root_markers = { "Application.cfc", ".git", ".vscode" },
 	settings = {},
